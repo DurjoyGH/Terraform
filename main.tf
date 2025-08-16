c47a1d64e148242e69893a8268578cf09d53dc77
DurjoyGH <durjoyghosh328@gmail.com>
2025-08-16 23:55:05 +0600

    Successful

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
-provider "aws" {
-    region = "eu-north-1"
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-042b4708b1d05f512"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "TerraformAssignment"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "durjoy-terraform-assignment-${random_id.bucket_id.hex}"
+
+  tags = {
+    Name = "TerraformAssignment"
+  }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
 }
